e641c21a5331328a69a96ed05b45e412a9188c89
anshu <anshu.sharma@netsolutions.in>
2015-04-28 10:32:19 +0530

    add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,49 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", " Ruby on Rails Tutorial Sample App"
   end
-
-  test "should get help" do
+test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
-
-  test "should get about" do
+test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
-
+def string_message(str = '')
+   if str.empty?
+    "It's an empty string!"
+  else
+     "The string is nonempty."
+   end
 end
+end
+#require 'test_helper'
+
+#class StaticPagesControllerTest < ActionController::TestCase
+
+  #def setup
+   # @base_title = "Ruby on Rails Tutorial Sample App"
+  #end
+
+  #test "should get home" do
+    #get :home
+    #assert_response :success
+    #assert_select "title", "Home | #{@base_title}"
+  #end
+
+  #test "should get help" do
+   # get :help
+   # assert_response :success
+    #assert_select "title", "Help | #{@base_title}"
+  #end
+
+  #test "should get about" do
+   # get :about
+    #assert_response :success
+   # assert_select "title", "About | #{@base_title}"
+ # end
+#end
