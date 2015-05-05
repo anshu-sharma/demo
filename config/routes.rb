673f0c6184ff32116c1b1d7f2327d744be185a0a
anshu <anshu.sharma@netsolutions.in>
2015-05-05 12:52:12 +0530

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -38,10 +38,15 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-
-  resources:account_activation, only:[:edit]
-  resources:password_resets,  only: [:new, :create, :edit, :update]
-  resources:microposts,          only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activation, only:[:edit]
+  resources :password_resets,  only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   # Example resource route with options:
   #   resources :products do
   #     member do
